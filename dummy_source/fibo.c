41204a206b3771562effcfa0bece10fe41717cf3
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 06:06:53 +0000

    Comments Added by LLM

--- dummy_source/fibo.c
+++ dummy_source/fibo.c
@@ -1,28 +1,18 @@
+
 #include <stdio.h>
+#include "twosum.h"
+
+
+/* add 2 numbers*/
 int main() {
+    int num1 = 5, num2 = 7;
 
-  int i, n;
+    
+    int result = sum(num1, num2);
 
-  // initialize first and second terms
-  int t1 = 0, t2 = 1;
+    printf("The sum of %d and %d is: %d
+", num1, num2, result);
 
-  // initialize the next term (3rd term)
-  int nextTerm = t1 + t2;
-
-  // get no. of terms from user
-  printf("Enter the number of terms: ");
-  scanf("%d", &n);
-
-  // print the first two terms t1 and t2
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-  // print 3rd to nth terms
-  for (i = 3; i <= n; ++i) {
-    printf("%d, ", nextTerm);
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-
-  return 0;
+    return 0;
 }
+
