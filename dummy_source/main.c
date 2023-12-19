f0b18d51871e0599737bd563794dab3eb9c65619
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 07:02:03 +0000

    Comments Added by LLM

--- dummy_source/main.c
+++ dummy_source/main.c
@@ -1,14 +1,34 @@
+  Here is the commented code:
+
 #include <stdio.h>
 #include "twosum.h"
 
 int main() {
     int num1 = 5, num2 = 7;
 
-    
+    /*
+     * The following line declares a variable called result and assigns it the value of the sum of num1 and num2.
+     * The sum function is called with the arguments num1 and num2, and the result is stored in the result variable.
+     */
     int result = sum(num1, num2);
 
+    /*
+     * The following line prints the result of the sum function to the console.
+     * The format specifier %d is used to print an integer value.
+     */
     printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
 
+#include "dummy.h"
+int sum(int a, int b) {
+    /*
+     * The following if statement checks if both a and b are greater than 0.
+     * If they are, the function returns the sum of a and b.
+     * If they are not, the function returns 0.
+     */
+    if (a > 0 && b > 0) {
+        return a + b; //
+    }
+}
