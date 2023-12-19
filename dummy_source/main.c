e1bf753e05c9290b7770913f52054829d5fc161f
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 07:00:44 +0000

    Comments Added by LLM

--- dummy_source/main.c
+++ dummy_source/main.c
@@ -1,14 +1,34 @@
+  Here is the commented code:
+
 #include <stdio.h>
 #include "twosum.h"
 
 int main() {
     int num1 = 5, num2 = 7;
 
-    
+    /*
+     * The following line calculates the sum of num1 and num2 using the sum function
+     * defined in the twosum.h header file.
+     */
     int result = sum(num1, num2);
 
+    /*
+     * The following line prints the result of the sum function to the console.
+     */
     printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
 
+#include "dummy.h"
+
+/*
+ * The sum function takes two integers as input and returns their sum.
+ * If both numbers are positive, the function returns their sum.
+ * If either number is negative, the function returns 0.
+ */
+int sum(int a, int b) {
+    if (a > 0 && b > 0) {
+        return a + b; //
+    }
+}
