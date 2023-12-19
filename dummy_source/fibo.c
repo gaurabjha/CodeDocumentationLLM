0c539da6be5f1a7315a0339903ad72ad7c54e0f9
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 06:15:44 +0000

    Comments Added by LLM

--- dummy_source/fibo.c
+++ dummy_source/fibo.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
-int main() {
+  ```
+    /*
+     * fibo.c
+     *
+     * Calculates the nth Fibonacci number using a recursive approach.
+     *
+     * Author: [Your Name]
+     * Date: [Today's Date]
+     */
 
-  int i, n;
+    #include <stdio.h>
 
-  // initialize first and second terms
-  int t1 = 0, t2 = 1;
+    int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 
-  // initialize the next term (3rd term)
-  int nextTerm = t1 + t2;
-
-  // get no. of terms from user
-  printf("Enter the number of terms: ");
-  scanf("%d", &n);
-
-  // print the first two terms t1 and t2
-  printf("Fibonacci Series: %d, %d, ", t1, t2);
-
-  // print 3rd to nth terms
-  for (i = 3; i <= n; ++i) {
-    printf("%d, ", nextTerm);
-    t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
-  }
-
-  return 0;
-}
+    int main() {
+        int n;
+        printf("Enter a positive integer: ");
+        scanf("%d", &n);
+        printf("The %dth Fibonacci number is %d\n", n, fibonacci(n));
+        return 0;
+    }
+    ```
