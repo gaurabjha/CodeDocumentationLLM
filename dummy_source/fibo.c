ff3f8b5a64710e8b30a07d412319da6064c5fc12
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 06:27:42 +0000

    Comments Added by LLM

--- dummy_source/fibo.c
+++ dummy_source/fibo.c
@@ -1,22 +1,24 @@
+  Here is the commented code:
+
 #include <stdio.h>
+
+// This program calculates the Fibonacci series up to a given number of terms.
+
 int main() {
 
+  // Declare variables
   int i, n;
-
-  // initialize first and second terms
   int t1 = 0, t2 = 1;
-
-  // initialize the next term (3rd term)
   int nextTerm = t1 + t2;
 
-  // get no. of terms from user
+  // Get the number of terms from the user
   printf("Enter the number of terms: ");
   scanf("%d", &n);
 
-  // print the first two terms t1 and t2
+  // Print the first two terms
   printf("Fibonacci Series: %d, %d, ", t1, t2);
 
-  // print 3rd to nth terms
+  // Print the remaining terms
   for (i = 3; i <= n; ++i) {
     printf("%d, ", nextTerm);
     t1 = t2;
@@ -25,4 +27,102 @@ int main() {
   }
 
   return 0;
+}
+
+// Function definitions
+
+// Check if a character is a JSON array
+bool json_is_array(char ch) {
+  return ch == '[';
+}
+
+// Check if a character is a JSON boolean
+bool json_is_boolean(char ch) {
+  return ch == 't' || ch == 'f';
+}
+
+// Check if a character is a JSON null
+bool json_is_null(char ch) {
+  return ch == 'n';
+}
+
+// Check if a character is a JSON number
+bool json_is_number(char ch) {
+  return (ch >= '0' && ch <= '9') || ch == '+' || ch == '-' || ch == '.' ||
+         ch == 'e' || ch == 'E';
+}
+
+// Check if a character is a JSON object
+bool json_is_object(char ch) {
+  return ch == '{';
+}
+
+// Check if a character is a JSON string
+bool json_is_string(char ch) {
+  return ch == '"';
+}
+
+// Find an element in a JSON object
+result(json_element) json_object_find(typed(json_object) * obj,
+                                       typed(json_string) key) {
+  if (key == NULL || strlen(key) == 0) {
+    return result_err(json_element)(JSON_ERROR_INVALID_KEY);
+  }
+
+  typed(uint64) bucket = json_key_hash(key) % obj->count;
+
+  for (size_t i = 0; i < obj->count; i++) {
+    typed(json_entry) *entry = obj->entries[bucket];
+    if (strcmp(key, entry->key) == 0) {
+      return result_ok(json_element)(entry->element);
+    }
+
+    bucket = (bucket + 1) % obj->count;
+  }
+
+  return result_err(json_element)(JSON_ERROR_INVALID_KEY);
+}
+
+// Skip a JSON array
+bool json_skip_array(typed(json_string) * str_ptr) {
+  // Skip the starting '[' character
+  (*str_ptr)++;
+
+  json_skip_whitespace(str_ptr);
+
+  // Unfortunately the array is empty
+  if (**str_ptr == ']') {
+    // Skip the end ']'
+    (*str_ptr)++;
+    return false;
+  }
+
+  while (**str_ptr != '\0') {
+    json_skip_whitespace(str_ptr);
+
+    // Guess the type
+    result(json_element_type) type_result = json_guess_element_type(*str_ptr);
+    if (result_is_ok(json_element_type)(&type_result)) {
+      typed(json_element_type) type =
+          result_unwrap(json_element_type)(&type_result);
+
+      // Parse the value based on guessed type
+      json_skip_element_value(str_ptr, type);
+
+      json_skip_whitespace(str_ptr);
+    }
+
+    // Reached the end
+    if (**str_ptr == ']') {
+      break;
+    }
+
+    // Skip the ','
+    (*str_ptr)++;
+  }
+
+  // Skip the ']' closing array
+  (*str_ptr)++;
+
+  return true;
 }
