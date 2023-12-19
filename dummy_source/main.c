095ccdf229ed2926b383e382e11137074a051c65
Gaurab Jha <Gaurab.Jha@wdc.com>
2023-12-19 06:42:08 +0000

    Comments Added by LLM

--- dummy_source/main.c
+++ dummy_source/main.c
@@ -1,14 +1,29 @@
+  Here is the commented code:
+
 #include <stdio.h>
 #include "twosum.h"
 
 int main() {
     int num1 = 5, num2 = 7;
 
-    
-    int result = sum(num1, num2);
+    // Declare variables
+    int result;
 
+    // Call the sum function
+    result = sum(num1, num2);
+
+    // Print the result
     printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
 
+// Pipeline added Funtion Definations
+
+int sum(int a, int b) {
+    // Check if both numbers are positive
+    if (a > 0 && b > 0) {
+        // Return the sum of the two numbers
+        return a + b;
+    }
+}
