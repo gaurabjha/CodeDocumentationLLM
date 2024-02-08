493ea159b0b25d50a3c0158a1e2aa31d0b9d1163
Gaurab Jha <GaurabKJha@gmail.com>
2024-02-08 11:03:48 +0530

    Update main.c

--- dummy_source/main.c
+++ dummy_source/main.c
@@ -3,15 +3,18 @@
 
 int main() {
     int num1 = 5, num2 = 7;
-
-    
+ 
+ 
+    // Get the Sum of the numbers
     int result = sum(num1, num2);
-
-    printf("The sum of %d and %d is: %d\n", num1, num2, result);
-
+ 
+    // Print the Sum of the numbers.
+    printf("The sum of %d and %d is: %d\n", num1, num2, result); // inline comment
+ 
     return 0;
 }
 
+// Dummy Function
 int sum2(int num1, int num2) {
     return num1 + num2;
 }
